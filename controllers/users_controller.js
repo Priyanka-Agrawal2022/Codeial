f9bcbaeac0145732a21c62c4513b22df5fa2cea7
Priyanka-Agrawal2022 <prnkgrwl286@gmail.com>
2023-02-19 02:02:40 +0530

    setup user schema and manual sign up authentication using cookies

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req, res) {
     // return res.end('<h1>User Profile</h1>');
     return res.render('user_profile', {
@@ -10,4 +12,51 @@ module.exports.posts = function(req, res) {
     return res.render('user_posts', {
         title: "Posts"
     });
+}
+
+// render the sign up page
+module.exports.signUp = function(req, res) {
+    return res.render('user_sign_up', {
+        title: "Codeial | Sign Up"
+    });
+}
+
+// render the sign in page
+module.exports.signIn = function(req, res) {
+    return res.render('user_sign_in', {
+        title: "Codeial | Sign In"
+    });
+}
+
+// get the sign up data
+module.exports.create = function(req, res) {
+    if(req.body.password != req.body.confirm_password) {
+        return res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email}, function(err, user) {
+        if(err) {
+            console.log('error in finding user in signing up');
+            return;
+        }
+
+        if(!user) {
+            User.create(req.body, function(err, user) {
+                if(err) {
+                    console.log('error in creating user in signing up');
+                    return;
+                }
+
+                return res.redirect('/users/sign_in');
+            });
+        }
+        else {
+            return res.redirect('back');
+        }
+    });
+}
+
+// sign in and create a session for the user
+module.exports.createSession = function(req, res) {
+    
 }
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,8 @@ const usersController = require('../controllers/users_controller');
 
 router.get('/profile', usersController.profile);
 router.get('/posts', usersController.posts);
+router.get('/sign_up', usersController.signUp);
+router.get('/sign_in', usersController.signIn);
+router.post('/create', usersController.create);
 
 module.exports = router;
