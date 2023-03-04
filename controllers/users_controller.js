6b1e6a1dfdc98582ed1c88f8664c7231db2e41d6
Priyanka-Agrawal2022 <prnkgrwl286@gmail.com>
2023-03-05 04:49:38 +0530

    setup and use passport library for authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -16,6 +16,10 @@ module.exports.posts = function(req, res) {
 
 // render the sign up page
 module.exports.signUp = function(req, res) {
+    if(req.isAuthenticated()) {
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_up', {
         title: "Codeial | Sign Up"
     });
@@ -23,6 +27,10 @@ module.exports.signUp = function(req, res) {
 
 // render the sign in page
 module.exports.signIn = function(req, res) {
+    if(req.isAuthenticated()) {
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_in', {
         title: "Codeial | Sign In"
     });
@@ -47,7 +55,7 @@ module.exports.create = function(req, res) {
                     return;
                 }
 
-                return res.redirect('/users/sign_in');
+                return res.redirect('/users/sign-in');
             });
         }
         else {
@@ -58,5 +66,15 @@ module.exports.create = function(req, res) {
 
 // sign in and create a session for the user
 module.exports.createSession = function(req, res) {
-    
+    return res.redirect('/');
+}
+
+module.exports.destroySession = function(req, res) {
+    req.logout(function(err) {
+        if(err) {
+            console.log('Error in signing out the user!');
+        }
+
+        return res.redirect('/');
+    });
 }
