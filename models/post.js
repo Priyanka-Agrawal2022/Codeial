f03fc9c0dc74eae1384201fe592db4f517bf30a1
Priyanka-Agrawal2022 <prnkgrwl286@gmail.com>
2023-03-09 03:00:30 +0530

    create schema for comments

--- models/post.js
+++ models/post.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+
 const postSchema = new mongoose.Schema({
     content: {
         type: String,
@@ -7,9 +8,15 @@ const postSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
-    }
-},
-{
+    },
+    // include the array of ids of all comments in this post schema itself
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'comment'
+        }
+    ]
+}, {
     timestamps: true
 });
 
