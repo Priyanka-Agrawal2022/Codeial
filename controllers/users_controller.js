3bdab9eb7ac1d1a53c727e1d98302445c312a739
Priyanka-Agrawal2022 <prnkgrwl286@gmail.com>
2023-03-11 02:48:58 +0530

    display user profile links

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,10 +1,13 @@
 const User = require('../models/user');
 
 module.exports.profile = function(req, res) {
-    // return res.end('<h1>User Profile</h1>');
-    return res.render('user_profile', {
-        title: "User Profile"
+    User.findById(req.params.id, function(err, user) {
+        return res.render('user_profile', {
+            title: "User Profile",
+            profile_user: user
+        });
     });
+    // return res.end('<h1>User Profile</h1>');
 }
 
 module.exports.posts = function(req, res) {
