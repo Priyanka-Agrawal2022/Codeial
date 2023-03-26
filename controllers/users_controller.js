e54c6cae34b13314f103db92050ccabec275314f
Priyanka-Agrawal2022 <prnkgrwl286@gmail.com>
2023-03-27 02:09:38 +0530

    add file upload functionality through multer

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const fs = require('fs');
+const path = require('path');
 
 // let's keep it same as before
 module.exports.profile = function(req, res) {
@@ -11,18 +13,53 @@ module.exports.profile = function(req, res) {
     // return res.end('<h1>User Profile</h1>');
 }
 
-module.exports.update = function(req, res) {
+module.exports.update = async function(req, res) {
+    // if(req.user.id == req.params.id) {
+    //     User.findByIdAndUpdate(req.params.id, req.body, function(err, user) {
+    //         req.flash('success', 'Updated!');
+    //         return res.redirect('back');
+    //     });
+    // }
+    // else {
+    //     req.flash('error', 'Unauthorized!');
+    //     return res.status(401).send('Unauthorized');
+    // }
+
     if(req.user.id == req.params.id) {
-        User.findByIdAndUpdate(req.params.id, req.body, function(err, user) {
-            req.flash('success', 'Updated!');
+        try {
+            let user = await User.findById(req.params.id);
+            User.uploadedAvatar(req, res, function(err) {
+                if(err) {
+                    console.log('***** Multer Error: ', err);
+                }
+
+                // console.log(req.file);
+                user.name = req.body.name;
+                user.email = req.body.email;
+
+                if(req.file) {
+                    if(user.avatar) {
+                        fs.unlinkSync(path.join(__dirname, '..', user.avatar));
+                    }
+
+                    // this is saving the path of the uploaded file into the avatar field in the user
+                    user.avatar = User.avatarPath + '/' + req.file.filename;
+                }
+
+                user.save();    
+                return res.redirect('back');
+            });
+        } catch(err) {
+            req.flash('error', err);
             return res.redirect('back');
-        });
+        }
     }
     else {
         req.flash('error', 'Unauthorized!');
         return res.status(401).send('Unauthorized');
     }
 }
+
 
 module.exports.posts = function(req, res) {
     // return res.end('<h1>User Posts</h1>');
--- index.js
+++ index.js
@@ -24,6 +24,9 @@ app.use(express.urlencoded());
 
 app.use(cookieParser());
 
+// make the uploads path available to the browser
+app.use('/uploads', express.static(__dirname + '/uploads'));
+
 app.use(express.static('./assets'));
 
 app.use(expressLayouts);
