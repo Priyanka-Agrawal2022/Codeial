7ea16d76d5062f38f7e669f503a2e65b85d1667c
Priyanka-Agrawal2022 <prnkgrwl286@gmail.com>
2023-03-28 00:33:41 +0530

    create delete API for posts without authentication and authorization

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,60 @@
-module.exports.index = function(req, res) {
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
+
+module.exports.index = async function(req, res) {
+    let posts = await Post.find({})
+        .sort('-createdAt')
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
+
     return res.status(200).json({
         message: "List of posts",
-        posts: []
+        posts: posts
     });
+}
+
+module.exports.destroy = async function(req, res) {
+    try {
+        let post = await Post.findById(req.params.id);
+
+        // .id means converting the object id into string
+        // if(post.user == req.user.id) {
+            post.remove();
+    
+            await Comment.deleteMany({post: req.params.id});
+
+            // if(req.xhr) {
+            //     return res.status(200).json({
+            //         data: {
+            //             post_id: req.params.id
+            //         },
+            //         message: "Post deleted!"
+            //     });
+            // }
+
+            // req.flash('success', 'Post and associated comments deleted!');
+
+            // return res.redirect('back');
+            return res.status(200).json({
+                message: "Post and associated comments deleted!"
+            });
+        // }
+        // else {
+        //     req.flash('error', 'You cannot delete this post!');
+        //     return res.redirect('back');
+        // }
+    } catch (err) {
+        console.log('*****Error', err);
+        // req.flash('error', err); 
+        // return res.redirect('back');
+
+        return res.status(500).json({
+            message: "Internal Server Error"
+        });
+    }
 }
